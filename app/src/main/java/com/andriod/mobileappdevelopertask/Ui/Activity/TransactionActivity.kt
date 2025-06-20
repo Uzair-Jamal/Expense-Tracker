8e54bbfba4000c2658e16c2322ae07e31cc40667
UzaairJamal <ingenious.uzair@gmail.com>
2025-06-20 16:43:21 +0500

    View Transactions done without filter(bonus).

--- app/src/main/java/com/andriod/mobileappdevelopertask/Ui/Activity/TransactionActivity.kt
+++ app/src/main/java/com/andriod/mobileappdevelopertask/Ui/Activity/TransactionActivity.kt
@@ -2,23 +2,47 @@ package com.andriod.mobileappdevelopertask.Ui.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.andriod.mobileappdevelopertask.R
+import com.andriod.mobileappdevelopertask.adapter.TransactionAdapter
 import com.andriod.mobileappdevelopertask.databinding.ActivityTransactionBinding
+import com.andriod.mobileappdevelopertask.viewModel.TransactionViewmodel
 
 class TransactionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTransactionBinding
+    private lateinit var transactionAdapter: TransactionAdapter
+    private lateinit var viewModel: TransactionViewmodel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTransactionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this)[TransactionViewmodel::class.java]
+
+        setupRecyclerView()
         binding.addTransaction.setOnClickListener {
             startActivity(Intent(this, AddTransactionActivity::class.java))
+        }
+    }
+
+    private fun setupRecyclerView(){
+        transactionAdapter = TransactionAdapter()
+
+        binding.transactionRv.apply {
+            layoutManager = LinearLayoutManager(this@TransactionActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = transactionAdapter
+        }
+
+        viewModel.allTransactions.observe(this) { transactions ->
+            Log.d("transaction", "All Transactions: ${transactions.size}, Transaction: $transactions")
+            transactionAdapter.differ.submitList(transactions)
         }
     }
 }
---
+++ app/src/main/java/com/andriod/mobileappdevelopertask/adapter/TransactionAdapter.kt
@@ -0,0 +1,71 @@
+package com.andriod.mobileappdevelopertask.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.andriod.mobileappdevelopertask.R
+import com.andriod.mobileappdevelopertask.databinding.ItemTransactionBinding
+import com.andriod.mobileappdevelopertask.entity.Transaction
+
+class TransactionAdapter: RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TransactionAdapter.TransactionViewHolder {
+        return TransactionViewHolder(
+            ItemTransactionBinding.inflate(
+                LayoutInflater.from(parent.context),parent,false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TransactionAdapter.TransactionViewHolder, position: Int) {
+        val currentTransaction = differ.currentList[position]
+
+        if(currentTransaction.type == "Income"){
+        holder.itemTransaction.typeTv.text = currentTransaction.type
+            holder.itemTransaction.typeTv.setTextColor(ContextCompat.
+            getColor(holder.itemView.context,R.color.income_color))
+        }else if(currentTransaction.type == "Expense"){
+            holder.itemTransaction.typeTv.text = currentTransaction.type
+            holder.itemTransaction.typeTv.setTextColor(ContextCompat.
+            getColor(holder.itemView.context,R.color.expense_color))
+        }
+        holder.itemTransaction.categoryTv.text = currentTransaction.category
+        holder.itemTransaction.dateTv.text = currentTransaction.date
+        holder.itemTransaction.descriptionTv.text = currentTransaction.description
+        if(currentTransaction.type == "Income"){
+        holder.itemTransaction.amountTv.text = "Rs. ${currentTransaction.amount}"
+            holder.itemTransaction.amountTv.setTextColor(ContextCompat.
+            getColor(holder.itemView.context,R.color.income_color))
+        }
+        else if(currentTransaction.type == "Expense"){
+            holder.itemTransaction.amountTv.text = "Rs. ${currentTransaction.amount}"
+            holder.itemTransaction.amountTv.setTextColor(ContextCompat.
+            getColor(holder.itemView.context,R.color.expense_color))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class TransactionViewHolder(val itemTransaction: ItemTransactionBinding) : RecyclerView.ViewHolder(itemTransaction.root)
+
+    private val differCallBack = object:DiffUtil.ItemCallback<Transaction>(){
+        override fun areItemsTheSame(oldItem: Transaction, newItem: Transaction): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.type == newItem.type &&
+                    oldItem.category == newItem.category &&
+                    oldItem.date == newItem.date
+        }
+
+        override fun areContentsTheSame(oldItem: Transaction, newItem: Transaction): Boolean {
+            return oldItem == newItem
+        }
+    }
+    val differ = AsyncListDiffer(this, differCallBack)
+}
